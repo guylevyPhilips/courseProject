380ab5e26f0e48bb4ee0a89c3a3a5416d1939d3b
guylevyphilips <guy.levy@philips.com>
2022-09-04 09:48:11 +0300

    update ngrx store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule} from '@angular/common/http';
 
+import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
-import { StoreModule } from '@ngrx/store';
 import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer';
 
 
--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -2,8 +2,30 @@ import { Action } from "@ngrx/store";
 import { Ingredient } from "src/app/shared/ingredient.model";
 
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
+export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
+export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
+export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
 
 export class AddIngredient implements Action {
-    readonly type: string = 'ADD_INGREDIENT';    
-    payload: Ingredient;
+    readonly type: string = ADD_INGREDIENT;    
+    
+    constructor(public payload: Ingredient) {}
+}
+
+export class AddIngredients implements Action {
+    type: string = ADD_INGREDIENTS;
+
+    constructor(public payload: Ingredient[]) {}
+}
+
+export class UpdateIngredient implements Action {
+    type: string = UPDATE_INGREDIENT;
+
+    constructor(public payload: {index: number, ingredient: Ingredient}) {}
+}
+
+export class DeleteIngredient implements Action {
+    type: string = DELETE_INGREDIENT;
+
+    constructor(public payload: number) {}
 }
